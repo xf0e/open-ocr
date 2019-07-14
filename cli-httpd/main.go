6b262d50660d9833e3dd397bf999a5672c396210
xf0e <admin@theguns.de>
2019-07-14 22:14:01 +0200

    [CGL] Status page rework

--- cli-httpd/main.go
+++ cli-httpd/main.go
@@ -78,8 +78,9 @@ func main() {
 
 	// any requests to root, just redirect to main page
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		text := `<!DOCTYPE html><html lang="en"> <head> <meta charset="utf-8"> <title>open-ocr</title><style>pre{ font-family: Fixedsys,Courier,monospace; padding: 0px; }</style> </head> <body>` +
-			`<h1>Open-ocr  ></h1> Need <a href="https://godoc.org/github.com/xf0e/open-ocr">docs</a>?<div><p>Status: RUNNING</p><p>Nice day to put slinkies on an escalator!</p> ` +
+		text := `<!DOCTYPE html><html lang="en"> <head> <meta charset="utf-8"> <title>open-ocr</title></head> <body>` +
+			`<section class="nes-container with-title">	<h2 class="title">Open-ocr  ></h2>` +
+			`<div><a class="nes-btn is-success" href="">RUNNING</a>` +
 			`<pre>  __   ; '.'  :
 |  +. :  ;   :   __
 .-+\   ':  ;   ,,-'  :
@@ -98,14 +99,23 @@ func main() {
 ::,,...,;:HB\   BBBBBBBBBBBBBBB
 ::,,...,;:HB \  BBBBBBBBBBBBBBB
 '+.,...,;:P'  \ BBBBBBBBBBBBBBB
-               \BBBBBBBBBBBBBBB</pre></div><p>proudly made with BASIC:)</p></body> </html>`
+               \BBBBBBBBBBBBBBB</pre><p>Nice day to put slinkies on an escalator!</p></div><p>proudly made with QBASIC:)</p>
+			<p>Need <a href="https://godoc.org/github.com/xf0e/open-ocr">docs</a>?</p></section>` +
+			`<style> html, body,{font-family: "Fixedsys,Courier,monospace";}; body {max-width: 960px; min-width: 320px;` +
+			`margin: 0 auto;}; section {margin-top: 3em; margin: 3em 1.5em 0 1.5em;}; section:last-of-type {margin-bottom: 3em;};` +
+			`li {margin-top: 0.8em;}.nes-container {position: relative; padding: 1.5rem 2rem; border-color: #000; border-style: solid;` +
+			`border-width: 4px;} .nes-container.with-title > .title {display: table;padding: 0 .5rem;margin: -1.8rem 0 1rem; font-size:` +
+			`1rem;background-color: #fff;}` +
+			`.nes-btn {border-image-slice: 2;border-image-width: 2;border-image-outset: 2;position: relative;border-style: solid;border-width: 4px;` +
+			`text-decoration: none;background-color: #92cc41;display: inline-block;padding: 6px 8px;color: #fff;` +
+			`border-image-source: url('data:image/svg+xml;utf8,<?xml version="1.0" encoding="UTF-8" ?><svg version="1.1" width="5" height="5" xmlns="http://www.w3.org/2000/svg"><path d="M2 1 h1 v1 h-1 z M1 2 h1 v1 h-1 z M3 2 h1 v1 h-1 z M2 3 h1 v1 h-1 z" fill="rgb(33,37,41)" /></svg>');}` +
+			`}</style>` +
+			`</body> </html>`
 		fmt.Fprintf(w, text)
 	})
 
 	//http.Handle("/ocr", ocrworker.NewOcrHttpHandler(rabbitConfig))
 	http.Handle("/ocr", prometheus.InstrumentHandler("open-ocr-httpd", ocrworker.NewOcrHttpHandler(rabbitConfig)))
-
-	//http.Handle("/ocr", phm.WrapHandler("open-ocr-httpd", ocrworker.NewOcrHttpHandler(rabbitConfig))
 
 	http.Handle("/ocr-file-upload", ocrworker.NewOcrHttpMultipartHandler(rabbitConfig))
 
