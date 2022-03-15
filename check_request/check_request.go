7c95dcc50d57f42ccc1922c79598738376c90db5
xf0e <droidlove@ya.ru>
2022-03-15 12:14:33 +0100

    [CODING] updated openAPI specification

---
+++ check_request/check_request.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+import (
+	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/getkin/kin-openapi/openapi3filter"
+	legacyrouter "github.com/getkin/kin-openapi/routers/legacy"
+)
+
+func main() {
+	ctx := context.Background()
+	loader := openapi3.Loader{Context: ctx}
+	doc, _ := loader.LoadFromFile("openapi3.0.yml")
+	_ = doc.Validate(ctx)
+	router, _ := legacyrouter.NewRouter(doc)
+	httpReq, _ := http.NewRequest(http.MethodGet, "/ocr", nil)
+
+	// Find route
+	route, pathParams, _ := router.FindRoute(httpReq)
+
+	// Validate request
+	requestValidationInput := &openapi3filter.RequestValidationInput{
+		Request:    httpReq,
+		PathParams: pathParams,
+		Route:      route,
+	}
+	if err := openapi3filter.ValidateRequest(ctx, requestValidationInput); err != nil {
+		panic(err)
+	}
+
+	var (
+		respStatus      = 200
+		respContentType = "application/json"
+		respBody        = bytes.NewBufferString(`{}`)
+	)
+
+	log.Println("Response:", respStatus)
+	responseValidationInput := &openapi3filter.ResponseValidationInput{
+		RequestValidationInput: requestValidationInput,
+		Status:                 respStatus,
+		Header:                 http.Header{"Content-Type": []string{respContentType}},
+	}
+	if respBody != nil {
+		data, _ := json.Marshal(respBody)
+		responseValidationInput.SetBodyBytes(data)
+	}
+
+	// Validate response.
+	if err := openapi3filter.ValidateResponse(ctx, responseValidationInput); err != nil {
+		panic(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.17
 
 require (
 	github.com/couchbaselabs/go.assert v0.0.0-20130325201400-cfb33e3a0dac
+	github.com/getkin/kin-openapi v0.92.0
 	github.com/prometheus/client_golang v1.12.1
 	github.com/rabbitmq/amqp091-go v1.3.0
 	github.com/rs/zerolog v1.26.0
@@ -13,11 +14,16 @@ require (
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/go-openapi/jsonpointer v0.19.5 // indirect
+	github.com/go-openapi/swag v0.19.5 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/mailru/easyjson v0.0.0-20190626092158-b2ccc519800e // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
 	github.com/prometheus/client_model v0.2.0 // indirect
 	github.com/prometheus/common v0.32.1 // indirect
 	github.com/prometheus/procfs v0.7.3 // indirect
 	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
 	google.golang.org/protobuf v1.26.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
