b7ae57802b41dcd7faf250dced56eec1ca4a48ef
artem <mail@artemil.de>
2025-08-08 15:57:28 +0200

    reverted changes to httpd

--- cli-httpd/main.go
+++ cli-httpd/main.go
@@ -1,9 +1,6 @@
-// main package for the Open-OCR HTTP daemon.
-// This package provides the main entry point for the HTTP server that handles OCR requests.
 package main
 
 import (
-	"context"
 	"crypto/tls"
 	"flag"
 	"fmt"
@@ -22,54 +19,32 @@ import (
 	ocrworker "github.com/xf0e/open-ocr"
 )
 
-var (
-	sha1ver   string
-	buildTime string
-	version   string
-)
+// This assumes that there is a worker running
+// To test it:
+// curl -X POST -H "Content-Type: application/json" -d '{"img_url":"http://localhost:8081/img","engine":0}' http://localhost:8081/ocr
 
-// config holds the application's configuration.
-type config struct {
-	httpPort uint
-	debug    bool
-	useHttps bool
-	keyFile  string
-	certFile string
-	rabbit   ocrworker.RabbitConfig
-}
+var (
+	sha1ver      string
+	buildTime    string
+	version      string
+	appStopLocal = false
+)
 
 func init() {
 	zerolog.TimeFieldFormat = time.StampMilli
+	// Default level is info, unless debug flag is present
 	zerolog.SetGlobalLevel(zerolog.InfoLevel)
 }
 
-// handleIndex serves the landing page.
-func handleIndex(w http.ResponseWriter, _ *http.Request) {
-	w.Header().Add("Strict-Transport-Security", "max-age=63072000; includeSubDomains")
-	// The 'ocrworker.AppStop' variable is used to check if the application is in the process of shutting down.
-	// 'ocrworker.TechnicalErrorResManager' indicates if there are technical errors with the resource manager.
-	text := ocrworker.GenerateLandingPage(ocrworker.AppStop, ocrworker.TechnicalErrorResManager, version)
-	_, _ = fmt.Fprint(w, text)
+func handleIndex(writer http.ResponseWriter, _ *http.Request) {
+	writer.Header().Add("Strict-Transport-Security", "max-age=63072000; includeSubDomains")
+	appStopLocal = ocrworker.AppStop
+	text := ocrworker.GenerateLandingPage(appStopLocal, ocrworker.TechnicalErrorResManager, version)
+	_, _ = fmt.Fprint(writer, text)
 }
 
-// createHTTPServer creates and configures an HTTP server.
-func createHTTPServer(cfg *config, ocrChain http.Handler) *http.Server {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", handleIndex)
-	mux.Handle("/ocr", ocrChain)
-	mux.Handle("/ocr-file-upload", ocrworker.NewOcrHttpMultipartHandler(&cfg.rabbit))
-	mux.Handle("/ocr-status", ocrworker.NewOcrHttpStatusHandler())
-	mux.Handle("/metrics", promhttp.Handler())
-
-	// Add pprof endpoints for debugging performance issues.
-	mux.HandleFunc("/debug/pprof/", pprof.Index)
-	mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
-	mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
-	mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
-	mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
-
+func makeServerFromMux(mux *http.ServeMux) *http.Server {
 	return &http.Server{
-		Addr:              fmt.Sprintf(":%d", cfg.httpPort),
 		ReadTimeout:       60 * time.Second,
 		ReadHeaderTimeout: 60 * time.Second,
 		WriteTimeout:      60 * time.Second,
@@ -78,112 +53,160 @@ func createHTTPServer(cfg *config, ocrChain http.Handler) *http.Server {
 	}
 }
 
-// waitForShutdown handles graceful shutdown of the server.
-func waitForShutdown(srv *http.Server) {
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
-	ssig := <-signals
+func makeHTTPServer(rabbitConfig *ocrworker.RabbitConfig, ocrChain http.Handler) *http.Server {
+	mux := &http.ServeMux{}
+	mux.HandleFunc("/", handleIndex)
+	mux.Handle("/ocr", ocrChain)
+	mux.Handle("/ocr-file-upload", ocrworker.NewOcrHttpMultipartHandler(rabbitConfig))
+	// api end point for getting orc request status
+	mux.Handle("/ocr-status", ocrworker.NewOcrHttpStatusHandler())
+	// expose metrics for prometheus
+	mux.Handle("/metrics", promhttp.Handler())
 
-	log.Info().Str("component", "OCR_HTTP").Str("signal", sig.String()).
-		Msg("Caught signal to terminate, shutting down gracefully.")
+	mux.HandleFunc("/debug/pprof/", pprof.Index)
+	mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+	mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
 
-	// Create a context with a timeout to allow for graceful shutdown.
-	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-	defer cancel()
-
-	// Signal other parts of the application to stop.
-	ocrworker.StopChan <- true
-
-	// Wait for in-flight requests to complete.
-	for {
-		if atomic.LoadUint32(&ocrworker.RequestTrackLength) == 0 {
-			log.Info().Str("component", "OCR_HTTP").
-				Msg("OCR queue is now empty. open-ocr http daemon will now exit.")
-			break
-		}
-		log.Info().Str("component", "OCR_HTTP").Uint32("Length of Requests", atomic.LoadUint32(&ocrworker.RequestTrackLength)).
-			Msg("In-flight requests queue is not empty. Waiting for requests to be processed.")
-		time.Sleep(10 * time.Second)
-	}
-
-	// Shutdown the server.
-	if err := srv.Shutdown(ctx); err != nil {
-		log.Error().Err(err).Msg("Error during server shutdown")
-	}
+	return makeServerFromMux(mux)
 }
 
 func main() {
-	var cfg config
-	var flgVersion bool
+	// defer profile.Start(profile.MemProfile).Stop()
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGTERM, syscall.SIGINT, syscall.SIGINT, syscall.SIGQUIT)
 
-	// Define and parse command-line flags.
-	flag.UintVar(&cfg.httpPort, "http_port", 8080, "The http port to listen on")
-	flag.BoolVar(&cfg.debug, "debug", false, "Enable debug logging")
-	flag.BoolVar(&flgVersion, "version", false, "Show version and exit")
-	flag.BoolVar(&cfg.useHttps, "usehttps", false, "Use HTTPS")
-	flag.StringVar(&cfg.keyFile, "keyfile", "", "Path to private key for HTTPS")
-	flag.StringVar(&cfg.certFile, "certfile", "", "Path to certificate file for HTTPS")
-
-	// Override default RabbitMQ config with command-line flags.
-	cfg.rabbit = ocrworker.DefaultConfigFlagsOverride(flag.CommandLine.Add)
-
-	flag.Parse()
-
-	if flgVersion {
-		fmt.Printf("version %s. Build on %s from git commit hash %s\n", version, buildTime, sha1ver)
-		return
-	}
-
-	if cfg.debug {
-		zerolog.SetGlobalLevel(zerolog.DebugLevel)
-	}
-
-	// Log the configuration without sensitive information.
-	logConfig := cfg
-	urlTmp, _ := url.Parse(logConfig.rabbit.AmqpAPIURI)
-	logConfig.rabbit.AmqpAPIURI = ocrworker.StripPasswordFromUrl(urlTmp)
-	urlTmp, _ = url.Parse(logConfig.rabbit.AmqpURI)
-	logConfig.rabbit.AmqpURI = ocrworker.StripPasswordFromUrl(urlTmp)
-	log.Info().Interface("parameters", logConfig).Msg("starting with parameters")
-
-	// Create the OCR processing chain.
-	ocrcChain := ocrworker.InstrumentHttpStatusHandler(ocrworker.NewOcrHttpHandler(&cfg.rabbit))
-
-	// Start the resource manager in a separate goroutine.
-	go ocrworker.SetResManagerState(&cfg.rabbit)
-
-	// Create and start the HTTP server.
-	srv := createHTTPServer(&cfg, ocrChain)
-
-	log.Info().Str("component", "OCR_HTTP").Str("listenAddr", srv.Addr).Msg("Starting listener...")
+	/*	if err := agent.Listen(agent.Options{}); err != nil {
+		log.Fatal()
+	}*/
 
 	go func() {
-		var err error
-		if cfg.useHttps {
-			if cfg.certFile == "" || cfg.keyFile == "" {
-				log.Fatal().Msg("HTTPS requires both a key and a certificate file.")
+		for sig := range signals {
+			log.Info().Str("component", "OCR_HTTP").Str("signal", sig.String()).
+				Msg("Caught signal to terminate, will not serve any further requests. Once the ocr queue is empty," +
+					" http daemon will terminate.")
+
+			ocrworker.StopChan <- true
+
+			for {
+				// as soon number of queued requests reaches zero, http daemon will exit
+				if atomic.LoadUint32(&ocrworker.RequestTrackLength) == 0 {
+					log.Info().Str("component", "OCR_HTTP").Str("signal", sig.String()).
+						Msg("ocr queue is now empty. open-ocr http daemon will now exit. You may stop workers now")
+					time.Sleep(20 * time.Second) // delay puffer for sending all requests back
+					break
+				}
+				ocrworker.RequestsTrack.Range(func(key, value interface{}) bool {
+					log.Info().Str("component", "OCR_HTTP").Msg("In-flight request " + fmt.Sprint(key))
+					return true
+				})
+				log.Info().Str("component", "OCR_HTTP").Uint32("Length of Requests", atomic.LoadUint32(&ocrworker.RequestTrackLength)).
+					Msg("In-flight requests queue is not empty. You can either wait until all request get processed(may take a long time), or just kill the process. Next check happens in 60 seconds.")
+				time.Sleep(60 * time.Second)
 			}
-			// Configure TLS settings for security.
-			srv.TLSConfig = &tls.Config{
-				MinVersion:       tls.VersionTLS12,
-				CurvePreferences: []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
-				CipherSuites: []uint16{
-					ls.TLS_AES_256_GCM_SHA384,
-					ls.TLS_CHACHA20_POLY1305_SHA256,
-					ls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
-					ls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-					ls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-				},
-			}
-			err = srv.ListenAndServeTLS(cfg.certFile, cfg.keyFile)
-		} else {
-			err = srv.ListenAndServe()
-		}
-		if err != nil && err != http.ErrServerClosed {
-			log.Fatal().Err(err).Str("component", "CLI_HTTP").Msg("Server failed to start")
+			os.Exit(0)
 		}
 	}()
 
-	// Wait for a shutdown signal.
-	waitForShutdown(srv)
+	var httpPort uint
+	var debug bool
+	var flgVersion bool
+	var useHttps bool
+	var keyFile string
+	var certFile string
+	flagFunc := func() {
+		flag.UintVar(
+			&httpPort,
+			"http_port",
+			8080,
+			"The http port to listen on, eg, 8081",
+		)
+		flag.BoolVar(
+			&debug,
+			"debug",
+			false,
+			"sets debug flag, program will print more messages",
+		)
+		flag.BoolVar(
+			&flgVersion,
+			"version",
+			false,
+			"show version and exit",
+		)
+		flag.BoolVar(
+			&useHttps,
+			"usehttps",
+			false,
+			"set to use secure connection",
+		)
+		flag.StringVar(
+			&keyFile,
+			"keyfile",
+			"",
+			"path to private key",
+		)
+		flag.StringVar(
+			&certFile,
+			"certfile",
+			"",
+			"path to certificate file",
+		)
+	}
+
+	rabbitConfig := ocrworker.DefaultConfigFlagsOverride(flagFunc)
+	if flgVersion {
+		fmt.Printf("version %s. Build on %s from git commit hash %s\n", version, buildTime, sha1ver)
+		os.Exit(0)
+	}
+	if debug {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
+	rabbitConfigTemp := rabbitConfig
+	urlTmp, _ := url.Parse(rabbitConfigTemp.AmqpAPIURI)
+	rabbitConfigTemp.AmqpAPIURI = ocrworker.StripPasswordFromUrl(urlTmp)
+	urlTmp, _ = url.Parse(rabbitConfigTemp.AmqpURI)
+	rabbitConfigTemp.AmqpURI = ocrworker.StripPasswordFromUrl(urlTmp)
+	log.Info().Interface("parameters", rabbitConfigTemp).Msg("trying to start with parameters")
+
+	ocrChain := ocrworker.InstrumentHttpStatusHandler(ocrworker.NewOcrHttpHandler(&rabbitConfig))
+	listenAddr := fmt.Sprintf(":%d", httpPort)
+
+	// start a goroutine which will run forever and decide if we have resources for incoming requests
+	go func() {
+		ocrworker.SetResManagerState(&rabbitConfig)
+	}()
+	log.Info().Str("component", "OCR_HTTP").Str("listenAddr", listenAddr).Msg("Starting listener...")
+
+	if useHttps {
+		if certFile == "" || keyFile == "" {
+			log.Fatal().Msg("usehttps flag only makes sense if both the private key and a certificate are available")
+		}
+		httpsSrv := makeHTTPServer(&rabbitConfig, ocrChain)
+		httpsSrv.Addr = listenAddr
+
+		// crypto settings
+		cryptSettings := &tls.Config{
+			MinVersion:       tls.VersionTLS12,
+			CurvePreferences: []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+			CipherSuites: []uint16{
+				tls.TLS_AES_256_GCM_SHA384,
+				tls.TLS_CHACHA20_POLY1305_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			},
+		}
+		httpsSrv.TLSConfig = cryptSettings
+
+		if err := httpsSrv.ListenAndServeTLS(certFile, keyFile); err != nil {
+			log.Fatal().Err(err).Str("component", "CLI_HTTP").Caller().Msg("cli_https has failed to start")
+		}
+	} else {
+		httpSrv := makeHTTPServer(&rabbitConfig, ocrChain)
+		httpSrv.Addr = listenAddr
+		if err := httpSrv.ListenAndServe(); err != nil {
+			log.Fatal().Err(err).Str("component", "CLI_HTTP").Caller().Msg("cli_http has failed to start")
+		}
+	}
 }
